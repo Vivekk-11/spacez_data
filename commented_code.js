b4cc52db58c7b356b03c2571f85723ca452e0094
vivek <chimnanivivek14@gmail.com>
2024-08-10 08:55:12 +0530

    removed properties which don't have photos

---
+++ commented_code.js
@@ -0,0 +1,247 @@
+// {
+//     "id": "vana-vista",
+//     "propertyId": "SSSS03",
+//     "name": "Vana Vista",
+//     "type": "Villa",
+//     "description": "Vana Vista is a luxury villa with spacious halls and greenery surroundings.",
+//     "noOfGuests": 10,
+//     "noOfBedrooms": 5,
+//     "noOfBeds": 6,
+//     "noOfBathrooms": 1,
+//     "rating": 5,
+//     "price": "3,499",
+//     "locationDescription": "Koramangala, a vibrant locality in Bangalore, India, is famed for its blend of residential and commercial spaces. Known for upscale restaurants, cafes, and shopping centers, it’s also a tech hub with numerous startups and co-working spaces.",
+//     "mapCoordinates": "12.927868515887448, 77.62291413986851",
+//     "lat": 12.927868515887448,
+//     "lng": 77.62291413986851,
+//     "isActive": true,
+//     "address": "578, 16th Main Rd, Koramangala 3 Block, Koramangala, Bengaluru, Karnataka 560034",
+//     "location": "Koramangala, Bengaluru",
+//     "city": "Bengaluru",
+//     "cancelationPolicy": "If it is more than 14 days before your check-in date: you can freely move your stay, or receive Spacez credits of the full value of your trip which you can spend on any future stay at any Spacez property. If it is less than 14 days before your check-in date: you can no longer move your trip. We will re-list your dates and attempt to re-book them. If we are successful, you will receive Spacez credit for whatever dates we are able to re-book. We allow you to re-sell or gift your dates if you have a colleague or friend who can make them. In this case, just let Concierge know their name and email. By booking, you agree and understand Spacez does not give refunds, only credits under the stated conditions.",
+//     "link": "https://www.airbnb.co.in/rooms/1208528062530460941?source_impression_id=p3_1723024853_P3Y77GX1L61j255W",
+//     "thingsToDo": [
+//       {
+//         "url": "https://www.treebo.com/blog/wp-content/uploads/2018/01/best-parks-in-bangalore_1.jpg",
+//         "name": "Xyz Park",
+//         "description": "Tourist Destination",
+//         "distance": "2 km"
+//       },
+//       {
+//         "url": "https://www.treebo.com/blog/wp-content/uploads/2018/01/best-parks-in-bangalore_1.jpg",
+//         "name": "Xyz Park",
+//         "description": "Tourist Destination",
+//         "distance": "2 km"
+//       }
+//     ],
+//     "amenities": [
+//       "hair-dryer",
+//       "shampoo",
+//       "dryer",
+//       "body-soap",
+//       "bidet",
+//       "shower-gel",
+//       "hot-water",
+//       "washing-machine",
+//       "bed-sheets",
+//       "towels",
+//       "toilet-paper",
+//       "bed-linen",
+//       "drying-rack",
+//       "iron",
+//       "air-conditioning",
+//       "wifi",
+//       "fire-extinguisher",
+//       "refrigerator",
+//       "free-parking",
+//       "balcony"
+//     ],
+//     "houseRules": [
+//       "check-in",
+//       "check-out",
+//       "self-check-in",
+//       "pets-allowed",
+//       "10-guests-maximum"
+//     ],
+//     "commonImages": [
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355180/image_1__final_zmy1ll.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355178/IMG_1492_final_bywi3m.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355172/20231106_170237-01_final_hdtjt0.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718352731/image_6__final_qgmerj.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355175/image_9__final_ftjqys.webp"
+//     ],
+//     "heroImages": [
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355174/IMG_1577_final_ztadx0.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355181/IMG_1458_final_mupkzv.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355175/20231106_171834-01_final_lvbosz.webp"
+//     ],
+//     "mainImage": "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355182/IMG_1438_final_dkjfgx.webp",
+//     "listingImage": "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355183/IMG_1074_final_tynkqg.webp",
+//     "photos": {}
+//   },
+
+// {
+//     "id": "stellar-skyline",
+//     "propertyId": "SSSS03",
+//     "name": "Stellar Skyline",
+//     "type": "Villa",
+//     "description": "Relax with the whole family at this peaceful place to stay.",
+//     "noOfGuests": 8,
+//     "noOfBedrooms": 3,
+//     "noOfBeds": 3,
+//     "noOfBathrooms": 4,
+//     "rating": 5,
+//     "price": "3,499",
+//     "locationDescription": "Toli Chowki, located in Hyderabad, India, is a culturally rich area known for its diverse community and vibrant markets. It's a bustling residential neighborhood with numerous eateries, historical sites, and close proximity to IT hubs like Gachibowli and Hitech City.",
+//     "mapCoordinates": "17.40498163723562, 78.40534018688918",
+//     "lat": 17.40498163723562,
+//     "lng": 78.40534018688918,
+//     "isActive": true,
+//     "address": "My Home Rainbow Apartments, Shaikpet Rd, Aziz Bagh Colony, Toli Chowki, Hyderabad, Telangana 500008",
+//     "location": "Toli Chowki, Hyderabad",
+//     "city": "Hyderabad",
+//     "cancelationPolicy": "If it is more than 14 days before your check-in date: you can freely move your stay, or receive Spacez credits of the full value of your trip which you can spend on any future stay at any Spacez property. If it is less than 14 days before your check-in date: you can no longer move your trip. We will re-list your dates and attempt to re-book them. If we are successful, you will receive Spacez credit for whatever dates we are able to re-book. We allow you to re-sell or gift your dates if you have a colleague or friend who can make them. In this case, just let Concierge know their name and email. By booking, you agree and understand Spacez does not give refunds, only credits under the stated conditions.",
+//     "link": "https://www.airbnb.co.in/rooms/1205752375611340863?source_impression_id=p3_1723023592_P3cLCwxEvmV4l9Nd",
+//     "thingsToDo": [
+//       {
+//         "url": "https://www.treebo.com/blog/wp-content/uploads/2018/01/best-parks-in-bangalore_1.jpg",
+//         "name": "Xyz Park",
+//         "description": "Tourist Destination",
+//         "distance": "2 km"
+//       },
+//       {
+//         "url": "https://www.treebo.com/blog/wp-content/uploads/2018/01/best-parks-in-bangalore_1.jpg",
+//         "name": "Xyz Park",
+//         "description": "Tourist Destination",
+//         "distance": "2 km"
+//       }
+//     ],
+//     "amenities": [
+//       "hair-dryer",
+//       "shampoo",
+//       "dryer",
+//       "body-soap",
+//       "bidet",
+//       "shower-gel",
+//       "hot-water",
+//       "washing-machine",
+//       "bed-sheets",
+//       "towels",
+//       "toilet-paper",
+//       "bed-linen",
+//       "drying-rack",
+//       "iron",
+//       "air-conditioning",
+//       "wifi",
+//       "fire-extinguisher",
+//       "refrigerator",
+//       "free-parking",
+//       "balcony",
+//       "tv"
+//     ],
+//     "houseRules": [
+//       "check-in",
+//       "check-out",
+//       "self-check-in",
+//       "pets-allowed",
+//       "8-guests-maximum"
+//     ],
+//     "commonImages": [
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355180/image_1__final_zmy1ll.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355178/IMG_1492_final_bywi3m.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355172/20231106_170237-01_final_hdtjt0.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718352731/image_6__final_qgmerj.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355175/image_9__final_ftjqys.webp"
+//     ],
+//     "heroImages": [
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355174/IMG_1577_final_ztadx0.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355181/IMG_1458_final_mupkzv.webp",
+//       "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355175/20231106_171834-01_final_lvbosz.webp"
+//     ],
+//     "mainImage": "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355182/IMG_1438_final_dkjfgx.webp",
+//     "listingImage": "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355183/IMG_1074_final_tynkqg.webp",
+//     "photos": {}
+//   },
+
+  // {
+    //   "id": "magnum-opus",
+    //   "propertyId": "SSSS03",
+    //   "name": "Magnum Opus",
+    //   "type": "Villa",
+    //   "description": "Magnum Opus is a luxury villa by Spacez with 5 bedrooms",
+    //   "noOfGuests": 10,
+    //   "noOfBedrooms": 5,
+    //   "noOfBeds": 5,
+    //   "noOfBathrooms": 5,
+    //   "rating": 5,
+    //   "price": "3,499",
+    //   "locationDescription": "Brookfield, a notable suburb in Bangalore, India, is known for its residential complexes and commercial hubs. Close to ITPL and major tech companies, it offers modern amenities, shopping centers, and well-developed infrastructure, making it a sought-after area.",
+    //   "mapCoordinates": "12.959167339260617, 77.71874228553092",
+    //   "lat": 12.959167339260617,
+    //   "lng": 77.71874228553092,
+    //   "isActive": true,
+    //   "address": "381, 8th Main Rd, 5th Cross, BEML Layout, Brookefield, Bengaluru, Karnataka 560066",
+    //   "location": "Brookefield, Bengaluru",
+    //   "city": "Bengaluru",
+    //   "cancelationPolicy": "If it is more than 14 days before your check-in date: you can freely move your stay, or receive Spacez credits of the full value of your trip which you can spend on any future stay at any Spacez property. If it is less than 14 days before your check-in date: you can no longer move your trip. We will re-list your dates and attempt to re-book them. If we are successful, you will receive Spacez credit for whatever dates we are able to re-book. We allow you to re-sell or gift your dates if you have a colleague or friend who can make them. In this case, just let Concierge know their name and email. By booking, you agree and understand Spacez does not give refunds, only credits under the stated conditions.",
+    //   "link": "https://www.airbnb.co.in/rooms/1199670203280553956?source_impression_id=p3_1723022180_P3sR4or5_OGELJY7",
+    //   "thingsToDo": [
+    //     {
+    //       "url": "https://www.treebo.com/blog/wp-content/uploads/2018/01/best-parks-in-bangalore_1.jpg",
+    //       "name": "Xyz Park",
+    //       "description": "Tourist Destination",
+    //       "distance": "2 km"
+    //     },
+    //     {
+    //       "url": "https://www.treebo.com/blog/wp-content/uploads/2018/01/best-parks-in-bangalore_1.jpg",
+    //       "name": "Xyz Park",
+    //       "description": "Tourist Destination",
+    //       "distance": "2 km"
+    //     }
+    //   ],
+    //   "amenities": [
+    //     "bathtub",
+    //     "hair-dryer",
+    //     "shampoo",
+    //     "dryer",
+    //     "body-soap",
+    //     "bidet",
+    //     "shower-gel",
+    //     "hot-water",
+    //     "washing-machine",
+    //     "bed-sheets",
+    //     "towels",
+    //     "toilet-paper",
+    //     "bed-linen",
+    //     "drying-rack",
+    //     "iron",
+    //     "air-conditioning",
+    //     "wifi",
+    //     "fire-extinguisher",
+    //     "refrigerator",
+    //     "free-parking",
+    //     "balcony"
+    //   ],
+    //   "houseRules": [
+    //     "check-in",
+    //     "check-out",
+    //     "self-check-in",
+    //     "pets-allowed",
+    //     "10-guests-maximum"
+    //   ],
+    //   "commonImages": [
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355180/image_1__final_zmy1ll.webp",
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355178/IMG_1492_final_bywi3m.webp",
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355172/20231106_170237-01_final_hdtjt0.webp",
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718352731/image_6__final_qgmerj.webp",
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355175/image_9__final_ftjqys.webp"
+    //   ],
+    //   "heroImages": [
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355174/IMG_1577_final_ztadx0.webp",
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355181/IMG_1458_final_mupkzv.webp",
+    //     "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355175/20231106_171834-01_final_lvbosz.webp"
+    //   ],
+    //   "mainImage": "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355182/IMG_1438_final_dkjfgx.webp",
+    //   "listingImage": "https://res.cloudinary.com/dz3tveb47/image/upload/v1718355183/IMG_1074_final_tynkqg.webp",
+    //   "photos": {}
+    // },
